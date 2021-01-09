0ed8bb7a7c26ec4e9987c3cc08e6a59275b7f744
VUTuanTrung <trung.lespace@gmail.com>
2021-01-09 18:42:06 +0100

    wokring with useRef, clean up the previous effect

--- ingredient-management/src/components/Ingredients/Search.js
+++ ingredient-management/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,31 +6,38 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState('');
+  const inputRef = useRef()
 
   useEffect(() => {
-    console.log('<Search> re-render demo');
-  })
+    const timer = setTimeout(() => {
+      if (filter === inputRef.current) {
+        const query = filter.length === 0
+          ? ''
+          : `?orderBy="title"&equalTo="${filter}"`;
+        fetch('https://react-hooks-udpate-d7642-default-rtdb.firebaseio.com/ingredients.json' + query)
+          .then((res) => (res.json()))
+          .then((resData) => {
+            const loadedIngredients = [];
+            for (const key in resData) {
+              loadedIngredients.push({
+                id: key,
+                title: resData[key].title,
+                amount: resData[key].amount,
+              });
+            }
 
-  useEffect(() => {
-    console.log('<Search> re-render load ingredients');
-    const query = filter.length === 0
-      ? ''
-      : `?orderBy="title"&equalTo="${filter}"`;
-    fetch('https://react-hooks-udpate-d7642-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then((res) => (res.json()))
-      .then((resData) => {
-        const loadedIngredients = [];
-        for (const key in resData) {
-          loadedIngredients.push({
-            id: key,
-            title: resData[key].title,
-            amount: resData[key].amount,
+            onLoadIngredients(loadedIngredients);
           });
-        }
+      }
+    }, 500);
 
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [filter, onLoadIngredients]);
+    // It's a function that will run right before thisa same useEffect functional run next time
+    // So not often this function is done, but before the next time it will run
+    return () => {
+      // Clear up the previous timer before the new effect is applied
+      clearTimeout(timer);
+    }
+  }, [filter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -38,6 +45,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={filter}
             onChange={(event) => setFilter(event.target.value)}
