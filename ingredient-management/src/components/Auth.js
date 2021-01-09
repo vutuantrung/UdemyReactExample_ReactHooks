d1000c5980e1ac59016ea1c668f165d397fb4ca5
VUTuanTrung <trung.lespace@gmail.com>
2021-01-09 21:57:07 +0100

    add AuthContext

--- ingredient-management/src/components/Auth.js
+++ ingredient-management/src/components/Auth.js
@@ -4,7 +4,7 @@ import Card from './UI/Card';
 import './Auth.css';
 
 const Auth = props => {
-  const loginHandler = () => {};
+  const loginHandler = () => { };
 
   return (
     <div className="auth">
